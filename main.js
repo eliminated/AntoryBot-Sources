66bc5601c73ad7c4ee261d73b27a1e83e099a163
Golden Smoothy <41362406+eliminated@users.noreply.github.com>
2022-07-19 09:54:22 +0800

    First running

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+// This is discordjs v14.3.0
+// All codes from v13 are probably outdated
+
 const d = require('discord.js');
 require('dotenv').config();
-const { token } = process.env.TOKEN;
+const { Client, GatewayIntentBits, Partials } = require('discord.js');
+const {readdirSync} = require('fs');
+
+// Create new client instance
+
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds], partials: [Partials.Channel]       // This is v14 only
+});
+
+// When the client is ready, run this code (only onces)
+client.once('ready', () => {
+    console.log('\'ready\' () => {');
+});
+
+// Login to Discord with your app's token
+client.login(process.env.TOKEN);
